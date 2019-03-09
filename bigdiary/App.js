24be070317aa2612e651ddc7927d0e4c9f5710f0
soorajc <soorajcmpm@gmail.com>
2019-03-09 21:41:03 +0530

    Completed: Routing of screens

--- bigdiary/App.js
+++ bigdiary/App.js
@@ -2,7 +2,6 @@
  * Diary App V1
  */
 
- import React from 'react';
  import {
    StackNavigator,
  } from 'react-navigation';
@@ -11,21 +10,29 @@
    Animated,
  } from 'react-native';
 
-import SplashScreen from './src/components/SplashScreen/SplashScreen';
+ import SplashScreen from './src/components/SplashScreen/SplashScreen';
+ import Home from './src/components/Home/Home';
+ import Login from './src/components/Login/Login';
+ import DetailView from './src/components/DetailView/DetailView';
+ import InputForm from './src/components/InputForm/InputForm';
 
-const transitionConfig = () => ({
-  transitionSpec: {
-    duration: 0,
-    timing: Animated.timing,
-    easing: Easing.step0,
-  },
-});
+ const transitionConfig = () => ({
+   transitionSpec: {
+     duration: 0,
+     timing: Animated.timing,
+     easing: Easing.step0,
+   },
+ });
 
-const App = StackNavigator({
-  SplashScreen: { screen: SplashScreen },
-}, {
-  transitionConfig,
-  headerMode: 'none',
-});
+ const App = StackNavigator({
+   SplashScreen: { screen: SplashScreen },
+   Login: { screen: Login },
+   Home: { screen: Home },
+   DetailView: { screen: DetailView },
+   InputForm: { screen: InputForm },
+ }, {
+   transitionConfig,
+   headerMode: 'none',
+ });
 
-export default App;
+ export default App;
